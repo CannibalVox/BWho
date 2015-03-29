fefdcef41259da8b7f289f6154f446ade1973375
CannibalVox <sdbaynham@gmail.com>
2015-03-29 16:27:17 -0500

    Fix redis ports again

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ config.web = {}
 
 config.logging = process.env.NODE_LOGGING || 1;
 config.logging_level = process.env.LOGGING_LEVEL || 'debug';
-config.redis.host = process.env.REDISCLOUD_URL || 'localhost:6379';
+config.redis.host = process.env.REDISCLOUD_URL || 'localhost';
+config.redis.port = process.env.REDISCLOUD_PORT || 6379;
 
 config.postgre.url = process.env.DATABASE_URL || 'localhost';
 
