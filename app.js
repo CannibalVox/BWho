5e2816f55575179d89f8f0716d25c1205a6bf204
CannibalVox <sdbaynham@gmail.com>
2015-04-01 02:23:55 -0500

    Oops, don't need that there.

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var url = require('url');
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var csurf = require('csurf');
 
 var routes = require('./routes/index');
 var dashboard = require('./routes/dashboard');
